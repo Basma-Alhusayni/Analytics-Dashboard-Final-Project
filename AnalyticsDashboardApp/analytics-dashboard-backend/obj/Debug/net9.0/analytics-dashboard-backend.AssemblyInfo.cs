6823f9f9294085ecab36f2e0a37a4dbff544e832
BasmaAlhusayni <tu4358236@taibahu.edu.sa>
2025-09-04 17:50:06 +0300

    Added database file

--- AnalyticsDashboardApp/analytics-dashboard-backend/obj/Debug/net9.0/analytics-dashboard-backend.AssemblyInfo.cs
+++ AnalyticsDashboardApp/analytics-dashboard-backend/obj/Debug/net9.0/analytics-dashboard-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("analytics-dashboard-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a9ef48027835f042adc328b9922e54c1211978d")]
 [assembly: System.Reflection.AssemblyProductAttribute("analytics-dashboard-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("analytics-dashboard-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
